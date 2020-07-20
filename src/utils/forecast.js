3c3bd8ca320d463a8660a243ab9c7c024455905a
Matias <matias.2234@hotmail.com>
2020-07-20 17:33:45 -0500

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=c473e42d94b7591d868705aae4962f50&query=' + latitude + ',' + longitude +'&units=f'
+    request ({url, json: true}, (error, {body}) =>{
+        if (error){
+            callback('Unable to connect to Forecast Service', undefined)
+        } else if (body.error) {  
+            callback('Unable to find location. Try another one.', undefined)
+        } else {
+            callback(undefined, {
+                description: body.current.weather_descriptions[0],
+                temperature: body.current.temperature,
+                feels_like: body.current.feelslike
+            })
+        }
+    })
+}
+
+module.exports = forecast
